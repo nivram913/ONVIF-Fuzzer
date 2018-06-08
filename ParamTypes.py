781181e794915419ffbc0b312eba522d89dff574
Vauge Marvin <marvin.vauge@smardtv.com>
2018-06-08 10:15:37 +0200

    Start adding parameters types with legal values

---
+++ ParamTypes.py
@@ -0,0 +1,252 @@
+ParamTypes = {
+    'AbsoluteOrRelativeTimeType': {
+        'legal_values': []
+    },
+    'anyURI': {
+        'legal_values': []
+    },
+    'AudioEncoding': {
+        'legal_values': ['G711', 'G726', 'AAC']
+    },
+    'AutoFocusMode': {
+        'legal_values': ['AUTO', 'MANUAL']
+    },
+    'AuxiliaryData': {
+        'legal_values': []
+    },
+    'BacklightCompensationMode': {
+        'legal_values': ['OFF', 'ON']
+    },
+    'base64Binary': {
+        'legal_values': []
+    },
+    'Base64DERencodedASN1Value': {
+        'legal_values': []
+    },
+    'boolean': {
+        'legal_values': ['true', 'false']
+    },
+    'CapabilityCategory': {
+        'legal_values': ['All', 'Analytics', 'Device', 'Events', 'Imaging', 'Media', 'PTZ']
+    },
+    'CertificateID': {
+        'legal_values': []
+    },
+    'CertificationPathID': {
+        'legal_values': []
+    },
+    'dateTime': {
+        'legal_values': []
+    },
+    'Decision': {
+        'legal_values': ['Granted', 'Denied']
+    },
+    'Description': {
+        'legal_values': []
+    },
+    'DiscoveryMode': {
+        'legal_values': ['Discoverable', 'NonDiscoverable']
+    },
+    'DNAttributeType': {
+        'legal_values': []
+    },
+    'DNAttributeValue': {
+        'legal_values': []
+    },
+    'DNSName': {
+        'legal_values': []
+    },
+    'Dot11Cipher': {
+        'legal_values': ['CCMP', 'TKIP', 'Any', 'Extended']
+    },
+    'Dot11PSK': {
+        'legal_values': []
+    },
+    'Dot11PSKPassphrase': {
+        'legal_values': []
+    },
+    'Dot11SecurityMode': {
+        'legal_values': ['None', 'WEP', 'PSK', 'Dot1X', 'Extended']
+    },
+    'Dot11SSIDType': {
+        'legal_values': []
+    },
+    'Dot11StationMode': {
+        'legal_values': ['Ad-hoc', 'Infrastructure', 'Extended']
+    },
+    'DotDecimalOID': {
+        'legal_values': []
+    },
+    'Duplex': {
+        'legal_values': ['Full', 'Half']
+    },
+    'duration': {
+        'legal_values': []
+    },
+    'DynamicDNSType': {
+        'legal_values': ['NoUpdate', 'ClientUpdates', 'ServerUpdates']
+    },
+    'EFlipMode': {
+        'legal_values': ['OFF', 'ON', 'Extended']
+    },
+    'ExposureMode': {
+        'legal_values': ['AUTO', 'MANUAL']
+    },
+    'ExposurePriority': {
+        'legal_values': ['LowNoise', 'FrameRate']
+    },
+    'FactoryDefaultType': {
+        'legal_values': ['Hard', 'Soft']
+    },
+    'float': {
+        'legal_values': []
+    },
+    'H264Profile': {
+        'legal_values': ['Baseline', 'Main', 'Extended', 'High']
+    },
+    'ImageStabilizationMode': {
+        'legal_values': ['OFF', 'ON', 'AUTO', 'Extended']
+    },
+    'int': {
+        'legal_values': []
+    },
+    'integer': {
+        'legal_values': []
+    },
+    'IPAddressFilterType': {
+        'legal_values': ['Allow', 'Deny']
+    },
+    'IPType': {
+        'legal_values': ['IPv4', 'IPv6']
+    },
+    'IPv4Address': {
+        'legal_values': []
+    },
+    'IPv6Address': {
+        'legal_values': []
+    },
+    'IPv6DHCPConfiguration': {
+        'legal_values': ['Auto', 'Stateful', 'Stateless', 'Off']
+    },
+    'IrCutFilterMode': {
+        'legal_values': ['ON', 'OFF', 'AUTO']
+    },
+    'JobToken': {
+        'legal_values': []
+    },
+    'KeyID': {
+        'legal_values': []
+    },
+    'ModeOfOperation': {
+        'legal_values': ['Idle', 'Active', 'Unknown']
+    },
+    'Mpeg4Profile': {
+        'legal_values': ['SP', 'ASP']
+    },
+    'Name': {
+        'legal_values': []
+    },
+    'NetworkHostType': {
+        'legal_values': ['IPv4', 'IPv6', 'DNS']
+    },
+    'NetworkInterfaceConfigPriority': {
+        'legal_values': []
+    },
+    'NetworkProtocolType': {
+        'legal_values': ['HTTP', 'HTTPS', 'RTSP']
+    },
+    'nonNegativeInteger': {
+        'legal_values': []
+    },
+    'OSDType': {
+        'legal_values': ['Text', 'Image', 'Extended']
+    },
+    'ParityBit': {
+        'legal_values': ['None', 'Even', 'Odd', 'Mark', 'Space', 'Extended']
+    },
+    'positiveInteger': {
+        'legal_values': []
+    },
+    'PTZPresetTourDirection': {
+        'legal_values': ['Forward', 'Backward', 'Extended']
+    },
+    'PTZPresetTourOperation': {
+        'legal_values': ['Start', 'Stop', 'Pause', 'Extended']
+    },
+    'PTZPresetTourState': {
+        'legal_values': ['Idle', 'Touring', 'Paused', 'Extended']
+    },
+    'QNameListType': {
+        'legal_values': []
+    },
+    'ReceiverMode': {
+        'legal_values': ['AutoConnect', 'AlwaysConnect', 'NeverConnect', 'Unknown']
+    },
+    'RecordingJobMode': {
+        'legal_values': []
+    },
+    'RecordingJobReference': {
+        'legal_values': []
+    },
+    'RecordingReference': {
+        'legal_values': []
+    },
+    'ReferenceToken': {
+        'legal_values': []
+    },
+    'RelayIdleState': {
+        'legal_values': ['closed', 'open']
+    },
+    'RelayLogicalState': {
+        'legal_values': ['active', 'inactive']
+    },
+    'RelayMode': {
+        'legal_values': ['Monostable', 'Bistable']
+    },
+    'ReverseMode': {
+        'legal_values': ['OFF', 'ON', 'AUTO', 'Extended']
+    },
+    'RotateMode': {
+        'legal_values': ['OFF', 'ON', 'AUTO']
+    },
+    'SetDateTimeType': {
+        'legal_values': ['Manual', 'NTP']
+    },
+    'StreamType': {
+        'legal_values': ['RTP-Unicast', 'RTP-Multicast']
+    },
+    'string': {
+        'legal_values': []
+    },
+    'SystemLogType': {
+        'legal_values': ['System', 'Access']
+    },
+    'token': {
+        'legal_values': []
+    },
+    'TrackReference': {
+        'legal_values': []
+    },
+    'TrackType': {
+        'legal_values': ['Video', 'Audio', 'Metadata', 'Extended']
+    },
+    'TransportProtocol': {
+        'legal_values': ['UDP', 'TCP', 'RTSP', 'HTTP']
+    },
+    'UserLevel': {
+        'legal_values': ['Administrator', 'Operator', 'User', 'Anonymous', 'Extended']
+    },
+    'VideoEncoding': {
+        'legal_values': ['JPEG', 'MPEG4', 'H264']
+    },
+    'WhiteBalanceMode': {
+        'legal_values': ['AUTO', 'MANUAL']
+    },
+    'WideDynamicMode': {
+        'legal_values': ['OFF', 'ON']
+    },
+    'XPathExpression': {
+        'legal_values': []
+    }
+}
+
